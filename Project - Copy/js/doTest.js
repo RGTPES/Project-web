356840087d951f84a62827cd607c6551e2e5259f
RGTPES <Maiduyanh7@gmail.com>
2025-04-11 22:59:14 +0700

    Delete Project - Copy directory

--- Project - Copy/js/doTest.js
+++
@@ -1,7 +0,0 @@
-let testList = JSON.parse(localStorage.getItem("testList")) || [];
-function renderQuizNumber(){
-let numberQuiz= testList[2].number;
-console.log(numberQuiz);
-
-let number_quiz= document.getElementById("number-quiz");
-}
