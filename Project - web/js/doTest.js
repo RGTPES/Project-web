45f0e6cb640400f4fa03aa88d7769db42afad43f
RGTPES <Maiduyanh7@gmail.com>
2025-04-11 22:59:48 +0700

    Add files via upload

---
+++ Project - web/js/doTest.js
@@ -0,0 +1,7 @@
+let testList = JSON.parse(localStorage.getItem("testList")) || [];
+function renderQuizNumber(){
+let numberQuiz= testList[2].number;
+console.log(numberQuiz);
+
+let number_quiz= document.getElementById("number-quiz");
+}
